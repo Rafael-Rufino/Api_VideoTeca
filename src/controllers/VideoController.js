77cf2d5adbb0ed98390e04adb2aea0324ce0938b
RafaelRufino <rafaelrufino.52@gmail.com>
2021-08-16 13:32:04 -0300

    criando funcionalidade de update, updateLike, usando middlewares

--- src/controllers/VideoController.js
+++ src/controllers/VideoController.js
@@ -8,7 +8,7 @@ module.exports = {
       const videos = await Video.find();
       return res.status(200).json({ videos });
     } catch (err) {
-      response.status(500).json({ error: err.message });
+      res.status(500).json({ error: err.message });
     }
   },
 
@@ -33,6 +33,53 @@ module.exports = {
       return res.status(201).json({ message: "Video create with sucesfully!" });
     } catch (err) {
       res.status(400).json({ error: err.message });
+    }
+  },
+
+  async update(request, response) {
+    const { title, link } = request.body;
+
+    if (!title && !link) {
+      return response
+        .status(400)
+        .json({ error: "You must inform a new title or a new link" });
+    }
+    if (title) response.video.title = title;
+    if (link) response.video.link = link;
+
+    try {
+      await response.video.save();
+      return response
+        .status(200)
+        .json({ message: "Video upload successfully!" });
+    } catch (err) {
+      response.status(500).json({ error: err.message });
+    }
+  },
+
+  async delete(request, response) {
+    try {
+      await response.video.remove();
+      return response
+        .status(200)
+        .json({ message: "Video delete successfully!" });
+    } catch (err) {
+      return response.status(500).json({ error: err.message });
+    }
+  },
+
+  async updateLike(request, response) {
+    response.video.liked = !response.video.liked;
+
+    try {
+      await response.video.save();
+      return response.status(200).json({
+        message: `Video ${
+          response.video.liked ? "liked" : "unliked"
+        } successfully`,
+      });
+    } catch (err) {
+      response.status(400).json({ error: err.message });
     }
   },
 };
